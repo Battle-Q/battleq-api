41ddc3b707358a29cbe347cfb55b62172b9705d2
rhtld123 <rhtld123@gmail.com>
2023-05-22 23:47:44 +0900

    feat: ConfigurationPropertiesScan 추가

--- src/main/java/com/study/battleq/BattleQApplication.java
+++ src/main/java/com/study/battleq/BattleQApplication.java
@@ -2,9 +2,11 @@ package com.study.battleq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 @EnableConfigurationProperties
 public class BattleQApplication {
 
