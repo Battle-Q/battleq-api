e69a4cd39196e727111c65175a85a1c464ab1863
rhtld123 <rhtld123@gmail.com>
2023-05-26 16:25:47 +0900

    feat: UsernameNotFoundException을 throw 하도록 변경

--- src/main/java/com/study/battleq/modules/user/service/CustomUserDetailsService.java
+++ src/main/java/com/study/battleq/modules/user/service/CustomUserDetailsService.java
@@ -3,6 +3,7 @@ package com.study.battleq.modules.user.service;
 import com.study.battleq.modules.user.domain.entity.BattleQUser;
 import com.study.battleq.modules.user.domain.entity.UserEntity;
 import com.study.battleq.modules.user.domain.repository.UserQueryRepository;
+import com.study.battleq.modules.user.domain.repository.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -20,8 +21,16 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        UserEntity userEntity = userQueryRepository.findByEmail(username);
+        UserEntity userEntity = findByEmail(username);
         return new BattleQUser(userEntity.getEmail(), userEntity.getPassword(), Collections.singleton(getGrantedAuthority(userEntity)), userEntity.getId(), userEntity.getNickname());
+    }
+
+    private UserEntity findByEmail(String username) {
+        try {
+            return userQueryRepository.findByEmail(username);
+        } catch (UserNotFoundException e) {
+            throw new UsernameNotFoundException("해당 회원이 존재하지 않습니다.");
+        }
     }
 
     private SimpleGrantedAuthority getGrantedAuthority(UserEntity userEntity) {
