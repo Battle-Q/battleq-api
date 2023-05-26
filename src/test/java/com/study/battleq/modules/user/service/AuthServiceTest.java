acaaab9150527a9089f605de5a79114a170d7119
rhtld123 <rhtld123@gmail.com>
2023-05-26 18:22:52 +0900

    test: 단위테스트로 변경

--- src/test/java/com/study/battleq/modules/user/service/AuthServiceTest.java
+++ src/test/java/com/study/battleq/modules/user/service/AuthServiceTest.java
@@ -1,42 +1,50 @@
 package com.study.battleq.modules.user.service;
 
-import com.study.battleq.modules.user.domain.entity.Authority;
-import com.study.battleq.modules.user.domain.entity.UserEntity;
-import com.study.battleq.modules.user.domain.repository.UserQueryRepository;
+import com.study.battleq.infrastructure.common.service.AuthenticationManagerService;
+import com.study.battleq.infrastructure.config.jwt.JwtTokenProvider;
 import com.study.battleq.modules.user.service.dto.TokenDto;
+import com.study.battleq.modules.user.service.exception.LoginFailedException;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.context.ActiveProfiles;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.BadCredentialsException;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
-@SpringBootTest
-@ActiveProfiles("local")
+@ExtendWith(MockitoExtension.class)
 class AuthServiceTest {
 
-    @Autowired
+    @InjectMocks
     AuthService authService;
-    @Autowired
-    PasswordEncoder passwordEncoder;
-    @MockBean
-    UserQueryRepository userQueryRepository;
+    @Mock
+    AuthenticationManagerService authenticationManagerService;
+    @Mock
+    JwtTokenProvider jwtTokenProvider;
 
     @Test
     void 로그인_처리() {
         //given
-        String email = "a@b.c";
-        String password = "pwd";
-        String encodedPassword = passwordEncoder.encode(password);
-        when(userQueryRepository.findByEmail(anyString())).thenReturn(UserEntity.of(email, "name", encodedPassword, "nick", Authority.ROLE_ADMIN));
+        when(authenticationManagerService.getAuthentication(anyString(), anyString())).thenReturn(null);
+        when(jwtTokenProvider.createToken(any())).thenReturn(TokenDto.of("a", "b"));
         //when
-        TokenDto tokenDto = authService.login(email, password);
+        TokenDto tokenDto = authService.login("email", "password");
         //then
-        assertNotNull(tokenDto.getAccessToken());
-        assertNotNull(tokenDto.getRefreshToken());
+        assertEquals("a", tokenDto.getAccessToken());
+        assertEquals("b", tokenDto.getRefreshToken());
+    }
+
+    @Test
+    void 로그인_실패() {
+        //given
+        when(authenticationManagerService.getAuthentication(anyString(), anyString())).thenThrow(BadCredentialsException.class);
+        //when
+        //then
+        assertThrows(LoginFailedException.class, () -> authService.login("email", "password"));
     }
 }
