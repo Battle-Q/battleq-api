44093392c137256598ad2cd3ee0da954e33af682
rhtld123 <rhtld123@gmail.com>
2023-05-26 18:30:34 +0900

    feat: UserRepository public으로 변경

--- src/main/java/com/study/battleq/modules/user/domain/repository/UserRepository.java
+++ src/main/java/com/study/battleq/modules/user/domain/repository/UserRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-interface UserRepository extends JpaRepository<UserEntity, Long> {
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     Optional<UserEntity> findByEmailAndDeletedAtIsNull(String email);
 }
