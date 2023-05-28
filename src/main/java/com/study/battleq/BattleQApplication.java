6009165c539a1c671a2828a9a6bf9cd7437f2746
rhtld123 <rhtld123@gmail.com>
2023-05-28 22:35:45 +0900

    feat: EnableJpaAuditing 추가

--- src/main/java/com/study/battleq/BattleQApplication.java
+++ src/main/java/com/study/battleq/BattleQApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 @ConfigurationPropertiesScan
 @EnableConfigurationProperties
