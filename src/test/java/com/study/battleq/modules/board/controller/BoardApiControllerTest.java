11e31742cab6e770d931000d93832c01a9788e36
HoYeonHwang <hoyun7443@gmail.com>
2023-05-29 23:45:41 +0900

    feat : 게시판 테스트 코드 연습

--- src/test/java/com/study/battleq/modules/board/controller/BoardApiControllerTest.java
+++ src/test/java/com/study/battleq/modules/board/controller/BoardApiControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -21,12 +22,11 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 @ActiveProfiles("local")
 @AutoConfigureMockMvc
 public class BoardApiControllerTest {
-
-    private static final String TITLE = "title1";
+    private static final String TITLE = "title";
     private static final String CONTENT = "content";
     private static final String CATEGORY = "freeBoard";
     private static final boolean IMPORTANCE = false;
-    private static final String WRITER = "user1";
+    private static final String WRITER = "user";
 
     @Autowired
     MockMvc mockMvc;
@@ -38,10 +38,12 @@ public class BoardApiControllerTest {
     EntityManager entityManager;
 
     @Test
-    @DisplayName("게시판 저장 성공 및 조회")
-    void 게시판_저장_성공_및_조회() throws Exception {
+    @DisplayName("게시판 저장 성공")
+    void 게시판_저장_성공() throws Exception {
+        System.out.println("◼◼◼◼◼ BoardEntity Save Start... ◼◼◼◼◼ ︎");
+
         //given
-        BoardEntity save = BoardEntity.builder()
+        BoardEntity bookEntity = BoardEntity.builder()
                 .title(TITLE)
                 .content(CONTENT)
                 .category(CATEGORY)
@@ -49,14 +51,35 @@ public class BoardApiControllerTest {
                 .writer(WRITER)
                 .build();
         // 저장
-        Long id = boardRepository.save(save).getId();
-        entityManager.clear();
-        //when
-        Optional<BoardEntity> boardEntity = boardRepository.findById(id);
-        if(!boardEntity.isPresent()){ // 조회값이 없을 경우
-            assertThrows(Exception.class, () ->boardRepository.findById(id));
-        }
+        BoardEntity saveEntity = boardRepository.save(bookEntity);
+
+        BoardEntity wrongEntity = BoardEntity.builder()
+                .title("dump")
+                .build();
         //then
-        assertEquals(TITLE,boardEntity.get().getTitle());
+        assertEquals(saveEntity, bookEntity);
+    }
+
+    @Test
+    @DisplayName("게시판 검색 성공")
+    void 게시판_검색_성공() throws Exception {
+        System.out.println("◼◼◼◼◼ BoardEntity Search Start... ◼◼◼◼◼ ︎");
+
+        //given
+        String searchParam = "title";
+        BoardEntity bookEntity = BoardEntity.builder()
+                .title(TITLE)
+                .content(CONTENT)
+                .category(CATEGORY)
+                .importance(IMPORTANCE)
+                .writer(WRITER)
+                .build();
+
+        //when
+        boardRepository.save(bookEntity);
+        List<BoardEntity> boardEntity = boardRepository.findByTitle(searchParam);
+
+        //then
+        assertEquals(TITLE, boardEntity.get(0).getTitle());
     }
 }
