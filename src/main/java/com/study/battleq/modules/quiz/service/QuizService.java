546426e816b6885042d64f66cf0c3428b64500ad
의탕 <ourjes@naver.com>
2023-05-30 01:08:18 +0900

    feat: 퀴즈 기본 구조 init (설정 미흡)

---
+++ src/main/java/com/study/battleq/modules/quiz/service/QuizService.java
@@ -0,0 +1,5 @@
+package com.study.battleq.modules.quiz.service;
+
+public interface QuizService {
+
+}
---
+++ src/main/java/com/study/battleq/modules/quiz/service/QuizServiceImpl.java
@@ -0,0 +1,7 @@
+package com.study.battleq.modules.quiz.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class QuizServiceImpl implements QuizService{
+}
