5476d32e68d65d1a1851b799fad237fa6e002eb4
JoEuiSang <ourjes@naver.com>
2023-06-02 21:48:49 +0900

    feat: 퀴즈 조회 구조 생성

--- src/main/java/com/study/battleq/modules/quiz/controller/v1/QuizControllerV1.java
+++ src/main/java/com/study/battleq/modules/quiz/controller/v1/QuizControllerV1.java
@@ -1,9 +1,23 @@
 package com.study.battleq.modules.quiz.controller.v1;
 
+import com.study.battleq.modules.quiz.domain.entity.QuizDto;
+import com.study.battleq.modules.quiz.service.QuizService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("api/v1/quiz")
 public class QuizControllerV1 {
+
+    private final QuizService quizService;
+
+    @GetMapping("/{quizId}")
+    public QuizDto getQuiz(@PathVariable("quizId") String quizId){
+        return quizService.getQuiz(quizId);
+    }
+
 }
