5211d79ced05e39ede663bb05cb62648d7f7f627
JoEuiSang <ourjes@naver.com>
2023-06-02 21:53:29 +0900

    feat: 퀴즈 조회 구조 생성

---
+++ src/main/java/com/study/battleq/modules/quiz/domain/entity/QuizType.java
@@ -0,0 +1,10 @@
+package com.study.battleq.modules.quiz.domain.entity;
+
+public enum QuizType {
+    CATCH_MIND("캐치마인드"),
+    SHORT_ANSWER("단답형"),
+    MULTIPLE_CHOICE("객관식");
+
+    QuizType(String quizType) {
+    }
+}
