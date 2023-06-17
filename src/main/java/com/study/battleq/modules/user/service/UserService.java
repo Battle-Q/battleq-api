189000135d3d79ed1e9f2aefcfb6cb9210d00469
rhtld123 <rhtld123@gmail.com>
2023-06-17 23:16:49 +0900

    refactor: 파일명 변경

--- src/main/java/com/study/battleq/modules/user/service/UserService.java
+++ src/main/java/com/study/battleq/modules/user/service/UserService.java
@@ -6,7 +6,7 @@ import com.study.battleq.modules.user.domain.repository.UserCommandService;
 import com.study.battleq.modules.user.domain.repository.UserQueryService;
 import com.study.battleq.modules.user.domain.repository.exception.UserNotFoundException;
 import com.study.battleq.modules.user.service.dto.UserSignupCommand;
-import com.study.battleq.modules.user.service.exception.AlreadyUserSignupException;
+import com.study.battleq.modules.user.service.exception.AlreadySignupException;
 import com.study.battleq.modules.user.service.usecase.UserSignupUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -22,18 +22,18 @@ public class UserService implements UserSignupUseCase {
 
     @Override
     public void signup(UserSignupCommand command) {
-        validateUserAlreadySignup(command.getEmail());
+        validateAlreadySignup(command.getEmail());
         UserEntity entity = UserEntity.of(command.getEmail(), command.getName(), passwordEncoder.encode(command.getPassword()), command.getNickname(), Authority.ROLE_STUDENT);
         userCommandService.save(entity);
     }
 
-    private void validateUserAlreadySignup(String email) {
+    private void validateAlreadySignup(String email) {
         try {
             userQueryService.findByEmail(email);
         } catch (UserNotFoundException e) {
             return;
         }
 
-        throw AlreadyUserSignupException.thrown();
+        throw AlreadySignupException.thrown();
     }
 }
