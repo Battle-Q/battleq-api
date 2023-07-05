aaf25b88c70c574541ab4d864d6b654af41972cf
JoEuiSang <ourjes@naver.com>
2023-07-05 21:48:13 +0900

    feat: 퀴즈타입 순서 변경

--- src/main/java/com/study/battleq/modules/quiz/domain/entity/QuizType.java
+++ src/main/java/com/study/battleq/modules/quiz/domain/entity/QuizType.java
@@ -1,12 +1,12 @@
 package com.study.battleq.modules.quiz.domain.entity;
 
 public enum QuizType {
-    CATCH_MIND("캐치마인드"),
     SHORT_ANSWER("단답형"),
-    MULTIPLE_CHOICE("객관식"),
     TRUE_OR_FALSE("OX퀴즈"),
-    MAKE_ORDER("순서맞추기"),
-    INITIAL("초성퀴즈");
+    MULTIPLE_CHOICE("객관식"),
+    INITIAL("초성퀴즈"),
+    CATCH_MIND("캐치마인드"),
+    MAKE_ORDER("순서맞추기");
 
     //생성자화
     private String description;
