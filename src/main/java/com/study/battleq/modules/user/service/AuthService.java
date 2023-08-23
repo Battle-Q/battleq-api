37f0eabccc46ddfd2fc09f6f56ab3574fb852dba
rhtld123 <rhtld123@gmail.com>
2023-08-24 00:22:11 +0900

    refactor: 불필요한 interface 제거

--- src/main/java/com/study/battleq/modules/user/service/AuthService.java
+++ src/main/java/com/study/battleq/modules/user/service/AuthService.java
@@ -7,65 +7,60 @@ import com.study.battleq.modules.user.domain.redis.UserRedisRepository;
 import com.study.battleq.modules.user.service.dto.TokenDto;
 import com.study.battleq.modules.user.service.exception.LoginFailedException;
 import com.study.battleq.modules.user.service.exception.RefreshTokenExpiredException;
-import com.study.battleq.modules.user.service.usecase.LoginUseCase;
-import com.study.battleq.modules.user.service.usecase.RefreshTokenUseCase;
+import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Service;
 
-import java.util.concurrent.TimeUnit;
-
 @Service
 @RequiredArgsConstructor
-public class AuthService implements LoginUseCase, RefreshTokenUseCase {
+public class AuthService {
 
-    private final AuthenticationManagerService authenticationManagerService;
-    private final JwtTokenProvider jwtTokenProvider;
-    private final JwtProperties properties;
-    private final UserRedisRepository userRedisRepository;
+  private final AuthenticationManagerService authenticationManagerService;
+  private final JwtTokenProvider jwtTokenProvider;
+  private final JwtProperties properties;
+  private final UserRedisRepository userRedisRepository;
 
 
-    @Override
-    public TokenDto login(String email, String password) {
-        Authentication authentication = getAuthentication(email, password);
-        TokenDto tokenDto = jwtTokenProvider.createToken(authentication);
-        saveRefreshToken(authentication.getName(), tokenDto);
-        return tokenDto;
+  public TokenDto login(String email, String password) {
+    Authentication authentication = getAuthentication(email, password);
+    TokenDto tokenDto = jwtTokenProvider.createToken(authentication);
+    saveRefreshToken(authentication.getName(), tokenDto);
+    return tokenDto;
+  }
+
+  private void saveRefreshToken(String email, TokenDto tokenDto) {
+    userRedisRepository.save(email, tokenDto.getRefreshToken(),
+        properties.getRefreshTokenExpireTime(),
+        TimeUnit.MILLISECONDS);
+  }
+
+  private Authentication getAuthentication(String email, String password) {
+    try {
+      return authenticationManagerService.getAuthentication(email, password);
+    } catch (AuthenticationException e) {
+      throw LoginFailedException.thrown();
     }
+  }
 
-    private void saveRefreshToken(String email, TokenDto tokenDto) {
-        userRedisRepository.save(email, tokenDto.getRefreshToken(),
-                properties.getRefreshTokenExpireTime(),
-                TimeUnit.MILLISECONDS);
+  public TokenDto refresh(String accessToken, String refreshToken) {
+    jwtTokenProvider.isValidateToken(refreshToken);
+    Authentication authentication = getAuthenticationByAccessToken(accessToken);
+    String refreshTokenByRedis = userRedisRepository.findByEmail(authentication.getName()).orElseThrow(RefreshTokenExpiredException::thrown);
+    if (!refreshTokenByRedis.equals(refreshToken)) {
+      throw new IllegalArgumentException("RefreshToken이 일치하지 않습니다.");
     }
+    TokenDto tokenDto = jwtTokenProvider.createToken(authentication);
+    saveRefreshToken(authentication.getName(), tokenDto);
+    return tokenDto;
+  }
 
-    private Authentication getAuthentication(String email, String password) {
-        try {
-            return authenticationManagerService.getAuthentication(email, password);
-        } catch (AuthenticationException e) {
-            throw LoginFailedException.thrown();
-        }
+  private Authentication getAuthenticationByAccessToken(String accessToken) {
+    try {
+      return jwtTokenProvider.getAuthentication(accessToken);
+    } catch (Exception e) {
+      throw new IllegalArgumentException("올바른 토큰이 아닙니다.");
     }
-
-    @Override
-    public TokenDto refresh(String accessToken, String refreshToken) {
-        jwtTokenProvider.isValidateToken(refreshToken);
-        Authentication authentication = getAuthenticationByAccessToken(accessToken);
-        String refreshTokenByRedis = userRedisRepository.findByEmail(authentication.getName()).orElseThrow(RefreshTokenExpiredException::thrown);
-        if (!refreshTokenByRedis.equals(refreshToken)) {
-            throw new IllegalArgumentException("RefreshToken이 일치하지 않습니다.");
-        }
-        TokenDto tokenDto = jwtTokenProvider.createToken(authentication);
-        saveRefreshToken(authentication.getName(), tokenDto);
-        return tokenDto;
-    }
-
-    private Authentication getAuthenticationByAccessToken(String accessToken) {
-        try {
-            return jwtTokenProvider.getAuthentication(accessToken);
-        } catch (Exception e) {
-            throw new IllegalArgumentException("올바른 토큰이 아닙니다.");
-        }
-    }
+  }
 }
--- src/main/java/com/study/battleq/modules/user/service/usecase/RefreshTokenUseCase.java
+++
@@ -1,7 +0,0 @@
-package com.study.battleq.modules.user.service.usecase;
-
-import com.study.battleq.modules.user.service.dto.TokenDto;
-
-public interface RefreshTokenUseCase {
-    TokenDto refresh(String accessToken, String refreshToken);
-}
