1f72ba9e321c21b167ce61f2d975c91af304154a
rhtld123 <rhtld123@gmail.com>
2023-08-24 00:19:43 +0900

    feat: embedded redis 선택적으로 사용할 수 있도록 변경

--- src/main/java/com/study/battleq/infrastructure/config/redis/LocalEmbeddedRedisConfiguration.java
+++ src/main/java/com/study/battleq/infrastructure/config/redis/LocalEmbeddedRedisConfiguration.java
@@ -1,6 +1,7 @@
 package com.study.battleq.infrastructure.config.redis;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -21,7 +22,8 @@ import java.io.InputStreamReader;
 
 @Profile("local")
 @Configuration
-public class LocalRedisConfiguration {
+@ConditionalOnProperty(name = "redis.embedded.enabled",havingValue = "true")
+public class LocalEmbeddedRedisConfiguration {
 
   @Value("${spring.redis.port}")
   private int redisPort;
@@ -30,6 +32,7 @@ public class LocalRedisConfiguration {
 
   @PostConstruct
   public void redisServer() throws Exception {
+    System.out.println("redisPort = " + redisPort);
     int port = isRedisRunning() ? findAvailablePort() : redisPort;
     redisServer = getRedisServer(port);
     redisServer.start();
